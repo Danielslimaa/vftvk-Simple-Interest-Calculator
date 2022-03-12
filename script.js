4c136a87ed8830a016bdd2090ae54c8de9c25703
Danielslimaa <58752424+Danielslimaa@users.noreply.github.com>
2022-03-11 23:07:30 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    yearInFuture = new Date().getFullYear()+parseInt(years.value);
+    if(principal.value <= 0){
+        result.innerHTML = "<p> Enter a positive number.</p>"
+        principal.focus()
+        return
+    } else {
+        let interest = principal.value * years.value * rate.value / 100;
+
+        result = document.getElementById("result")
+        result.innerHTML = "<p>If you deposit <mark>" + principal.value + "</mark>,<br>an interest rate of <mark>"
+            + rate.value + "%</mark>.<br>You will receive an amount of <mark>"
+            + interest + "</mark>,<br>in the year <mark>" + yearInFuture + "</mark></p>";
+    }
 }
-        
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
